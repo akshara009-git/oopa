e909e003187d2a12be40eb8afde25ff3fe2f709e
akshara009-git <2420030260@klh.edu.in>
2025-11-09 16:27:29 +0530

    Create TypeConversion_Narrowing.java

---
+++ practicalPrograms/TypeConversion_Narrowing.java
@@ -0,0 +1,13 @@
+package practicalPrograms;
+
+public class TypeConversion_Narrowing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double b = 19.99;
+		int i = (int)b;
+		System.out.println("Double value: "+b);
+		System.out.println("Integer value: "+i);
+	}
+
+}
