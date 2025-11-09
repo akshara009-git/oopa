efa41619a368bd444932d039619de93d80801242
akshara009-git <2420030260@klh.edu.in>
2025-11-09 16:23:20 +0530

    Create ArithmeticOperations.java

---
+++ practicalPrograms/ArithmeticOperations.java
@@ -0,0 +1,22 @@
+package practicalPrograms;
+
+import java.util.*;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+        sc.close();
+	}
+
+}
